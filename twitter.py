f5b73ebbbd054ed63430efb2e9243e3a52f7bc73
jamessam <jamessam@users.noreply.github.com>
2017-07-01 23:01:05 -0700

    additional work before first push

--- twitter.py
+++ twitter.py
@@ -5,16 +5,22 @@ from TwitterAPI import TwitterAPI
 from keys import consumer_key, consumer_secret, access_key, access_secret
 
 def get_tweet_count(user):
-    r = api.request('statuses/user_timeline',
-        { 'screen_name': user, 'count': 1 })
+    arguments = { 'screen_name': user, 'count': 1 }
+    r = api.request('statuses/user_timeline', arguments)
     tweet = r.json()[0]
-    # with open('test.txt', 'w') as test:
-    #     test.write(json.dumps(tweet, indent=4, sort_keys=True))
-    return tweet['user']['statuses_count']
+    return tweet['user']['statuses_count'], tweet['id']
+
+def get_200_tweets(user, max_id):
+    arguments = { 'screen_name': user, 'count': 200, 'max_id': max_id }
+    r = api.request('statuses/user_timeline', arguments)
+    tweets = r.json()
+    return tweets
 
 def main():
-    tweet_count = get_tweet_count('jamessamsf')
-    print(tweet_count)
+    tweet_count, max_id = get_tweet_count('jamessamsf')
+
+    # TODO: FIGURE OUT HOW TO ITERATE OVER THE TIMELINE IN CHUNKS. DO I NEED TO
+    # FIND THE FIRST AND LAST TWEET IDs?
 
 if __name__ == '__main__':
     api = TwitterAPI(consumer_key, consumer_secret, access_key, access_secret)
